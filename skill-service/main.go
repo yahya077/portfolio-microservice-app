a427a3d8366b0eefd0ea3c85d28dca45410078c0
yahya077 <yahyahindioglu@gmail.com>
2023-02-18 15:37:29 +0300

    skill-service added

---
+++ skill-service/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"io"
+	"os"
+)
+
+func main() {
+	app := fiber.New()
+
+	api := app.Group("/api")
+
+	v1 := api.Group("/v1")
+
+	v1.Get("/get-skill", func(ctx *fiber.Ctx) error {
+		result := make(map[string]interface{})
+
+		jsonFile, err := os.Open("./data/skill.json")
+		data, _ := io.ReadAll(jsonFile)
+		defer jsonFile.Close()
+
+		if err != nil {
+			panic(err)
+		}
+
+		_ = json.Unmarshal(data, &result)
+
+		return ctx.JSON(result)
+	})
+
+	_ = app.Listen(fmt.Sprintf(":%s", os.Getenv("SERVICE_PORT")))
+}
